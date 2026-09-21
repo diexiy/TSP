a7308334e3cbbcd3b2c16c02634d58fef4c0e007
diexiy <mariabiirtm@gmail.com>
2026-09-21 20:38:42 +0200

    added generate cities function and started on an unfinished calculate distance

---
+++ cities.py
@@ -0,0 +1,25 @@
+import random
+
+def generate_cities(n):
+    cities = {} # cities är en tom diconary, INTE en lista 
+
+    for i in range(n):
+        letter = chr(65 + i) # A = letter 65 och chr() är en inbyggd Python-funktion som tar ett heltal och tolkar det som en Unicode-kodpunkt
+        x_coordinate = random.randint(0, 100)
+        y_coordinate = random.randint(0, 100)
+
+
+        cities[letter] = (x_coordinate, y_coordinate)
+    return cities
+
+
+
+#def distance(city1, city2, cities): 
+ #   int distance 
+    
+
+
+cities = generate_cities(10)
+print(cities)
+    
+    
